74ec5cc1c46dd833d1257573bdf768fd2207faeb
thiyagab <1@thiyagab.com>
2021-03-15 15:18:27 +0530

    Firebase integration started.. Code may not work properly

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,48 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ipltrumpcards/model/TrumpModel.dart';
 import 'package:ipltrumpcards/ui/GamePlay.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-      ChangeNotifierProvider(
-        create: (context) => TrumpModel(),
-        child: MyApp(),
-      ),
-    );
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TrumpModel(),
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cricket Cards',
-      theme: ThemeData(primarySwatch: Colors.blue, canvasColor: Colors.white),
-      home: GamePlay(title: 'Cricket Cards'),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text("Error");
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Cricket Cards',
+            theme: ThemeData(
+                primarySwatch: Colors.blue, canvasColor: Colors.white),
+            home: GamePlay(title: 'Cricket Cards'),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Text("Loading...");
+      },
     );
   }
 }
