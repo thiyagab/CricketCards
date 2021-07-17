32ddea125577a33be6d3c67608648e2d8966c905
thiyagab <1@thiyagab.com>
2021-07-18 00:45:00 +0530

    cloud function for weekly score

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -19,8 +20,50 @@ exports.updateUser = functions.firestore
         console.log(newValue)
         console.log(previousValue)
         return change.after.ref.set(previousValue, {merge: true});
+    }else{
+        let diff = newValue.score-previousValue.score;
+        if(diff >0){
+            newValue.weekscore=newValue.weekscore+diff;
+            return change.after.ref.set(newValue,{merge:true});
+        }
     }
     //...therefore update the document as.
     // admin.firestore().collection('test').doc(docId).update(snapshot.after.data());
 
 });
+admin.initializeApp();
+
+exports.scheduledFunction = functions.pubsub.schedule('*/1 * * * *')
+.timeZone('Asia/Kolkata')
+.onRun(async (context) => {
+    const cSnap = await admin.firestore().collection('teams').get();
+    const batch = admin.firestore().batch();
+    let maxscore =0;
+    let maxid='';
+    let championship=0;
+    cSnap.forEach(docSnap => {
+        if (!docSnap.data()) { return; }
+        if(docSnap.data().weekscore>maxscore){
+            maxscore=docSnap.data().weekscore;
+            maxid=docSnap.id;
+            championship=docSnap.data().championship
+        }
+        const ref = admin.firestore().doc(`teams/${docSnap.id}`);
+        batch.update(ref,{weekscore:0});
+       
+    });
+    if(maxid){
+        console.log(`Max score: ${maxscore} ${maxid}`);
+        cSnap.forEach(docSnap => {
+            const ref = admin.firestore().doc(`teams/${docSnap.id}`);
+            if( docSnap.id ==maxid){
+                batch.update(ref,{championship:championship+1,champion:true});
+            }else{
+                batch.update(ref,{champion:false});
+            }
+        });
+    }   
+   
+    batch.commit();
+    return null;
+  });
